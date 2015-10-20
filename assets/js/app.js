bd618a15307053343ced23bae102de91540dbf28
Arthur Gouveia <arthur.gouveia@gmail.com>
2015-10-20 17:45:27 -0300

    Removing amount of colors and adding a text on an image

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 var blendOptions = document.querySelectorAll('#blend-options li a'),
   blendTexts = document.querySelectorAll('.blend-text span'),
+  blendTextOnImage = document.querySelector('#text-on-image span'),
   fontColor = document.querySelector('#font-color');
 
 for (var i = blendOptions.length - 1; i >= 0; i--) {
@@ -10,6 +11,8 @@ fontColor.addEventListener('keyup', function() {
   for (var i = blendTexts.length - 1; i >= 0; i--) {
     blendTexts[i].style.color = this.value;
   }
+
+  blendTextOnImage.style.color = this.value;
 });
 
 applyBlendMode('difference');
@@ -22,4 +25,6 @@ function applyBlendMode(mixBlendMode) {
   for (var i = blendTexts.length - 1; i >= 0; i--) {
     blendTexts[i].style.mixBlendMode = mixBlendMode;
   }
+
+  blendTextOnImage.style.mixBlendMode = mixBlendMode;
 }
