ee61a478af720d8303f3251d05e5932389a9fbbb
Arthur Gouveia <arthur.gouveia@gmail.com>
2015-10-20 20:32:51 -0300

    Some code refactoring and enhancements on the UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,54 @@
-var blendOptions = document.querySelectorAll('#blend-options li a'),
-  blendTexts = document.querySelectorAll('.blend-text span'),
-  blendTextOnImage = document.querySelector('#text-on-image span'),
-  fontColor = document.querySelector('#font-color');
+(function(){
+  'use strict';
 
-for (var i = blendOptions.length - 1; i >= 0; i--) {
-  blendOptions[i].addEventListener('click', applyBlendMode);
-}
+  var blendOptions = document.querySelectorAll('#blend-options li a'),
+      blendTexts = document.querySelectorAll('.blend-text span'),
+      imageText = document.querySelector('#image-text'),
+      imageContainer = document.querySelector('#image-container'),
+      imageChangeButton = document.querySelector('#image-change-button'),
+      fontColorLevels = document.querySelectorAll('.font-color-levels'),
+      fontColorSample = document.querySelector('#font-color-sample'),
+      fontColorLevelRed = document.querySelector('#font-color-level-red'),
+      fontColorLevelGreen = document.querySelector('#font-color-level-green'),
+      fontColorLevelBlue = document.querySelector('#font-color-level-blue');
 
-fontColor.addEventListener('keyup', function() {
-  for (var i = blendTexts.length - 1; i >= 0; i--) {
-    blendTexts[i].style.color = this.value;
+  for (var i = blendOptions.length - 1; i >= 0; i--) {
+    blendOptions[i].addEventListener('click', applyBlendMode);
   }
 
-  blendTextOnImage.style.color = this.value;
-});
-
-applyBlendMode('difference');
-
-function applyBlendMode(mixBlendMode) {
-  if (typeof mixBlendMode !== 'string') {
-    var mixBlendMode = mixBlendMode.currentTarget.innerHTML;
+  for (var i = fontColorLevels.length - 1; i >= 0; i--) {
+    fontColorLevels[i].addEventListener('change', changeTextColor);
   }
 
-  for (var i = blendTexts.length - 1; i >= 0; i--) {
-    blendTexts[i].style.mixBlendMode = mixBlendMode;
+  imageChangeButton.addEventListener('click', function() {
+    imageContainer.style.background = "url('http://lorempixel.com/500/500?v=" + Date.now() + "')";
+    imageContainer.style.backgroundSize = 'cover';
+  });
+
+  applyBlendMode('difference');
+
+  function applyBlendMode(mixBlendMode) {
+    if (typeof mixBlendMode !== 'string') {
+      var mixBlendMode = mixBlendMode.currentTarget.innerHTML;
+    }
+
+    for (var i = blendTexts.length - 1; i >= 0; i--) {
+      blendTexts[i].style.mixBlendMode = mixBlendMode;
+    }
+
+    imageText.style.mixBlendMode = mixBlendMode;
   }
 
-  blendTextOnImage.style.mixBlendMode = mixBlendMode;
-}
+  function changeTextColor() {
+    var color = 'rgb(' + fontColorLevelRed.value +
+                ', ' + fontColorLevelGreen.value +
+                ', ' + fontColorLevelBlue.value + ')';
+
+    for (var i = blendTexts.length - 1; i >= 0; i--) {
+      blendTexts[i].style.color = color;
+    }
+
+    imageText.style.color = color;
+    fontColorSample.style.background = color;
+  }
+}());
